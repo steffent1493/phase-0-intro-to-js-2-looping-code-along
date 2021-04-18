972c0ecb83fdcd93337918c822cfc440a1b6db10
steffent1493 <steffent1493@gmail.com>
2021-04-18 19:32:08 -0400

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const thankYouMessage = [];
+const giftGiver = [];
+const giftEvent = [];
+
+function writeCards(giftGiver, giftEvent) {
+    for (let i = 0; i < giftGiver.length; i ++) {
+        thankYouMessage.push(`Thank you, ${giftGiver[i]}, for the wonderful ${giftEvent} gift!`);
+
+    };
+    return thankYouMessage;
+};
+
+writeCards(giftGiver, giftEvent);
+
+function countDown(x) {
+    while (x >= 0) {
+        console.log(x--);
+    };
+};
+countDown(10)
